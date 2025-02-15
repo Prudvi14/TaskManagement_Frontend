983a579896b42918df29335caf3d807d7eda551e
Likhilesh <likbalpande@example.com>
2025-02-15 17:57:36 +0530

    login page HW

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,17 @@
 const LoginPage = () => {
+    const handleLogin = async (e) => {
+        // H.W. --> login
+    };
     return (
         <div>
             <h1>Login Page</h1>
+            <form onSubmit={handleLogin}>
+                <input type="email" placeholder="Email" name="email" required />
+                {/* Uncontrolled Inputs */}
+                <input type="password" placeholder="Password" name="password" required />
+                {/* Uncontrolled Inputs */}
+                <button>Login</button>
+            </form>
         </div>
     );
 };
