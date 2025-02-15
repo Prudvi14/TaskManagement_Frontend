1d13009330a186ef966128da2981401e7d153c2e
Likhilesh <likbalpande@example.com>
2025-02-15 12:59:52 +0530

    BE integration :: sending OTP :: in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import { useState } from "react";
 
 const App = () => {
+    const [currUser, setCurrUser] = useState({
+        isLoggedIn: false,
+        fullName: "Guest",
+    });
+
     return (
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/sign-up" element={<SignUpPage />} />
+                    <Route path="/" element={currUser.isLoggedIn ? <HomePage /> : <Navigate to="/sign-up" />} />
+                    <Route path="/login" element={currUser.isLoggedIn ? <Navigate to="/" /> : <LoginPage />} />
+                    <Route path="/sign-up" element={currUser.isLoggedIn ? <Navigate to="/" /> : <SignUpPage />} />
                 </Routes>
             </BrowserRouter>
         </div>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
+import { Link } from "react-router";
+
 const SignUpPage = () => {
+    const [isOtpSent, setIsOtpSent] = useState(false);
+    const [email, setEmail] = useState(false);
+    const [fullName, setFullName] = useState(false);
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+    };
+
+    const handleSendOtp = async (e) => {
+        try {
+            e.preventDefault();
+            // you can do some validation on email using regex (H.W.)
+            const resp = await fetch(import.meta.env.VITE_BACKEND_URL + "/otps", {
+                method: "POST",
+                body: JSON.stringify({
+                    email: e.target.userEmail.value,
+                }),
+                headers: {
+                    "content-type": "application/json",
+                },
+            });
+
+            console.log(resp);
+            const respObj = await resp.json();
+            console.log(respObj);
+        } catch (err) {
+            alert(err.message);
+        }
+    };
+
     return (
         <div>
-            <h1>Sign-up Page</h1>
+            {isOtpSent ? (
+                <form onSubmit={handleRegister}>
+                    <input type="text" value={email} readOnly />
+                    {/* Controlled Inputs */}
+                    <input type="text" value={fullName} readOnly />
+                    {/* Controlled Inputs */}
+                    <input type="text" placeholder="OTP" name="otp" required />
+                    {/* Uncontrolled Inputs */}
+                    <input type="password" placeholder="Password" name="password" required />
+                    {/* Uncontrolled Inputs */}
+                    <input type="password" placeholder="Confirm Password" name="confirmPassword" required />
+                    {/* Uncontrolled Inputs */}
+                    <button>Register</button>
+                </form>
+            ) : (
+                <form onSubmit={handleSendOtp}>
+                    <input type="text" placeholder="Full Name" name="fullName" required />
+                    {/* Uncontrolled Inputs */}
+                    <input type="email" placeholder="Email" name="userEmail" required />
+                    {/* Uncontrolled Inputs */}
+                    <button>Send OTP</button>
+                </form>
+            )}
+            <Link to="/login">Login</Link>
         </div>
     );
 };
