dffea41cee9cdfea7d9e533ce8089ab9cdf71f52
Likhilesh <likbalpande@example.com>
2025-02-15 17:57:27 +0530

    registration integration done

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 const SignUpPage = () => {
     const [isOtpSent, setIsOtpSent] = useState(false);
-    const [email, setEmail] = useState(false);
-    const [fullName, setFullName] = useState(false);
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const navigate = useNavigate();
 
-    const handleRegister = (e) => {
-        e.preventDefault();
-    };
-
-    const handleSendOtp = async (e) => {
+    const handleRegister = async (e) => {
         try {
             e.preventDefault();
-            // you can do some validation on email using regex (H.W.)
-            const resp = await fetch(import.meta.env.VITE_BACKEND_URL + "/otps", {
+
+            // do password validation using regex that it must contain ...
+            // add some more input fields in registration and store it in database
+
+            if (e.target.password.value !== e.target.confirmPassword.value) {
+                alert("Password does not match!");
+                return;
+            }
+
+            const resp = await fetch(import.meta.env.VITE_BACKEND_URL + "/users/register", {
                 method: "POST",
                 body: JSON.stringify({
-                    email: e.target.userEmail.value,
+                    email,
+                    fullName,
+                    otp: e.target.otp.value,
+                    password: e.target.password.value,
                 }),
                 headers: {
                     "content-type": "application/json",
@@ -27,6 +35,44 @@ const SignUpPage = () => {
             console.log(resp);
             const respObj = await resp.json();
             console.log(respObj);
+            if (respObj.status === "success") {
+                // use hook to change the page useNavigate
+                navigate("/login");
+            } else {
+                alert(respObj.message);
+            }
+        } catch (err) {
+            alert(err.message);
+        }
+    };
+
+    const handleSendOtp = async (e) => {
+        try {
+            e.preventDefault();
+            // you can do some validation on email using regex (H.W.)
+
+            // console.log(e.target[0].value, e.target[1].value);
+            // console.log(e.target.fullName.value, e.target.userEmail.value);
+            const resp = await fetch(import.meta.env.VITE_BACKEND_URL + "/otps", {
+                method: "POST",
+                body: JSON.stringify({
+                    email: e.target.userEmail.value,
+                }), // javascript object to json --> JSON.stringify()
+                headers: {
+                    "content-type": "application/json",
+                },
+            });
+
+            const respObj = await resp.json();
+            // console.log(resp);
+            // console.log(respObj);
+            if (respObj.status === "success") {
+                setIsOtpSent(true);
+                setFullName(e.target.fullName.value);
+                setEmail(e.target.userEmail.value);
+            } else {
+                alert("Error " + respObj.message);
+            }
         } catch (err) {
             alert(err.message);
         }
@@ -37,9 +83,9 @@ const SignUpPage = () => {
             {isOtpSent ? (
                 <form onSubmit={handleRegister}>
                     <input type="text" value={email} readOnly />
-                    {/* Controlled Inputs */}
+                    {/* Controlled Inputs but we already have the value */}
                     <input type="text" value={fullName} readOnly />
-                    {/* Controlled Inputs */}
+                    {/* Controlled Inputs but we already have the value */}
                     <input type="text" placeholder="OTP" name="otp" required />
                     {/* Uncontrolled Inputs */}
                     <input type="password" placeholder="Password" name="password" required />
